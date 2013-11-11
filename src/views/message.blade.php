25c6b34362cc2763d01a25be78f2e6e151277390
Graham Campbell <graham@mineuk.com>
2013-11-11 00:09:18 +0000

    Minor improvement

--- src/views/message.blade.php
+++ src/views/message.blade.php
@@ -4,5 +4,5 @@
 <p>{{ $name }} has sent you a message through the contact form on <a href="{{ $url }}">{{ Config::get('cms.name') }}</a>.<p>
 <p>Their message was:<p>
 <blockquote>{{ $quote }}</blockquote>
-<p>You may contact them again via their email address: {{ $contact }}<p>
+<p>You may contact them via their email address: <a href="mailto:{{ $contact }}">{{ $contact }}</a>.<p>
 @stop
